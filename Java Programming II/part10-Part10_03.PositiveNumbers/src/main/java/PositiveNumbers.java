8f23b7fb0f12ef378b1a47b5edb8358dcac5b704
stevenkha <60914699+stevenkha@users.noreply.github.com>
2020-08-11 15:05:10 -0700

    Exercise regarding collection streams. Outputs all positive numbers in a list

---
+++ Java Programming II/part10-Part10_03.PositiveNumbers/src/main/java/PositiveNumbers.java
@@ -0,0 +1,20 @@
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class PositiveNumbers {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // test your method here
+
+    }
+    
+    public static List<Integer> positive(List<Integer> numbers) {
+        List<Integer> positives = numbers.stream().filter(value -> value > 0).collect(Collectors.toCollection(ArrayList::new));
+        return positives;
+    }
+
+}
